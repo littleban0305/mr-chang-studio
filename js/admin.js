cdff7e84abc439b7cfebb008d27a09c97c0bd293
littleban0305 <littleban0305@gmail.com>
2026-07-19 21:56:42 +0800

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,76 @@
+if(
+    !localStorage.getItem(
+        "isAdmin"
+    )
+){
 
+    location.href =
+    "admin-login.html";
+
+}
+
+const bookingDate =
+localStorage.getItem(
+    "bookingDate"
+);
+
+const bookingDateText =
+document.getElementById(
+    "adminBookingDate"
+);
+
+if(
+    bookingDate
+){
+
+    bookingDateText.textContent =
+    bookingDate;
+
+}
+
+const statusText =
+document.getElementById(
+    "adminStatus"
+);
+
+document
+.getElementById(
+    "confirmBtn"
+)
+.addEventListener(
+    "click",
+    ()=>{
+
+        statusText.textContent =
+        "已確認";
+
+    }
+);
+
+document
+.getElementById(
+    "waitingBtn"
+)
+.addEventListener(
+    "click",
+    ()=>{
+
+        statusText.textContent =
+        "候補中";
+
+    }
+);
+
+document
+.getElementById(
+    "cancelBtn"
+)
+.addEventListener(
+    "click",
+    ()=>{
+
+        statusText.textContent =
+        "已取消";
+
+    }
+);
