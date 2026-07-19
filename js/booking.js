1818128508593b9dccd06e40fe59dae2ff41840e
littleban0305 <littleban0305@gmail.com>
2026-07-19 16:50:18 +0800

    Create booking.js

---
+++ js/booking.js
@@ -0,0 +1,19 @@
+const bookBtn =
+document.getElementById(
+    "bookBtn"
+);
+
+if(bookBtn){
+
+    bookBtn.addEventListener(
+        "click",
+        ()=>{
+
+            alert(
+                "預約成功（測試）"
+            );
+
+        }
+    );
+
+}
