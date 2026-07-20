cfa42cfc911816863f73e2811d5ccb042176d79b
littleban0305 <littleban0305@gmail.com>
2026-07-20 08:13:37 +0800

    Update my-bookings.js

--- js/my-bookings.js
+++ js/my-bookings.js
@@ -72,3 +72,34 @@ if(cancelBtn){
     );
 
 }
+
+const bookingCreatedAtText =
+document.getElementById(
+    "bookingCreatedAt"
+);
+
+if(
+    bookingCreatedAt &&
+    bookingCreatedAtText
+){
+
+    const date =
+    new Date(
+        bookingCreatedAt
+    );
+
+    bookingCreatedAtText.textContent =
+    date.toLocaleString(
+        "zh-TW",
+        {
+            year:"numeric",
+            month:"2-digit",
+            day:"2-digit",
+            hour:"2-digit",
+            minute:"2-digit",
+            second:"2-digit",
+            fractionalSecondDigits:3
+        }
+    );
+
+}
